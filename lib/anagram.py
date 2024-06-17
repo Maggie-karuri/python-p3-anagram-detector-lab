9adc5bd803ee62054810b551373018450b6304fd
Maggie-Karuri <karurimargaret111@gmail.com>
2024-06-17 13:16:36 +0300

    list comparison

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = ''.join(sorted(word))
+    
+    def match(self, words):
+        return [word for word in words if ''.join(sorted(word)) == self.sorted_word]
